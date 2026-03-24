58f18adad614eb4b485c6b3fc0ce9370f6496441
Fatomartyr <fatomartyr@gmail.com>
2026-03-24 23:43:51 +0300

    Update bat

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 chcp 65001 >nul
+
 if exist "build" rmdir /s /q "build"
 if exist "dist\LimbusAutoluxer" rmdir /s /q "dist\LimbusAutoluxer"
 
@@ -8,11 +9,26 @@ pyinstaller ^
     --name "LimbusAutoluxer" ^
     --windowed ^
     --icon=NONE ^
+    --hidden-import=pynput.mouse ^
+    --hidden-import=pynput.keyboard ^
+    --hidden-import=pydirectinput ^
+    --hidden-import=mss ^
+    --hidden-import=cv2 ^
+    --hidden-import=numpy ^
+    --hidden-import=pygetwindow ^
+    --collect-all=tkinter ^
     app.py
+
+if %errorlevel% neq 0 (
+    echo Ошибка сборки
+    pause
+    exit /b 1
+)
 
 if exist "battle-images" (
     xcopy "battle-images" "dist\LimbusAutoluxer\battle-images" /E /I /Y
-
+) else (
+    echo Папка battle-images не найдена!
 )
 
 pause
